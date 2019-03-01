60142fb72a86f36fa2f301a2fcdba4b49e822383
richardvtbtcp <>
2019-03-01 14:39:35 +1100

    Update build.sh

--- btcputil/build.sh
+++ btcputil/build.sh
@@ -19,13 +19,13 @@ gprefix READLINK readlink
 cd "$(dirname "$("$READLINK" -f "$0")")/.."
 
 # Allow user overrides to $MAKE. Typical usage for users who need it:
-#   MAKE=gmake ./btcputil/build.sh -j$(nproc)
+#   MAKE=gmake ./zcutil/build.sh -j$(nproc)
 if [[ -z "${MAKE-}" ]]; then
     MAKE=make
 fi
 
 # Allow overrides to $BUILD and $HOST for porters. Most users will not need it.
-#   BUILD=i686-pc-linux-gnu ./btcputil/build.sh
+#   BUILD=i686-pc-linux-gnu ./zcutil/build.sh
 if [[ -z "${BUILD-}" ]]; then
     BUILD="$(./depends/config.guess)"
 fi
@@ -46,32 +46,25 @@ Usage:
 $0 --help
   Show this help message and exit.
 
-$0 [ --enable-lcov || --disable-tests ] [ --disable-mining ] [ --disable-rust ] [ --enable-proton ] [ --disable-libs ] [ MAKEARGS... ]
-  Build Bitcoin Private and most of its transitive dependencies from
-  source. MAKEARGS are applied to both dependencies and Bitcoin Private itself.
+$0 [ --enable-lcov || --disable-tests ] [ --disable-mining ] [ --enable-proton ] [ MAKEARGS... ]
+  Build Zcash and most of its transitive dependencies from
+  source. MAKEARGS are applied to both dependencies and Zcash itself.
 
-  If --enable-lcov is passed, Bitcoin Private is configured to add coverage
+  If --enable-lcov is passed, Zcash is configured to add coverage
   instrumentation, thus enabling "make cov" to work.
-  If --disable-tests is passed instead, the Bitcoin Private tests are not built.
+  If --disable-tests is passed instead, the Zcash tests are not built.
 
-  If --disable-mining is passed, Bitcoin Private is configured to not build any mining
+  If --disable-mining is passed, Zcash is configured to not build any mining
   code. It must be passed after the test arguments, if present.
 
-  If --disable-rust is passed, Bitcoin Private is configured to not build any Rust language
-  assets. It must be passed after test/mining arguments, if present.
-
-  If --enable-proton is passed, Bitcoin Private is configured to build the Apache Qpid Proton
+  If --enable-proton is passed, Zcash is configured to build the Apache Qpid Proton
   library required for AMQP support. This library is not built by default.
-  It must be passed after the test/mining/Rust arguments, if present.
-
-  If --disable-libs is passed, Bitcoin Private is configured to not build any libraries like
-  'libzcashconsensus'.
+  It must be passed after the test/mining arguments, if present.
 EOF
     exit 0
 fi
 
 set -x
-cd "$(dirname "$(readlink -f "$0")")/.."
 
 # If --enable-lcov is the first argument, enable lcov coverage support:
 LCOV_ARG=''
@@ -96,14 +89,6 @@ then
     shift
 fi
 
-# If --disable-rust is the next argument, disable Rust code:
-RUST_ARG=''
-if [ "x${1:-}" = 'x--disable-rust' ]
-then
-    RUST_ARG='--enable-rust=no'
-    shift
-fi
-
 # If --enable-proton is the next argument, enable building Proton code:
 PROTON_ARG='--enable-proton=no'
 if [ "x${1:-}" = 'x--enable-proton' ]
@@ -112,19 +97,7 @@ then
     shift
 fi
 
-# If --disable-libs is the next argument, build without libs:
-LIBS_ARG=''
-if [ "x${1:-}" = 'x--disable-libs' ]
-then
-    LIBS_ARG='--without-libs'
-    shift
-fi
-
-PREFIX="$(pwd)/depends/$BUILD/"
-
 eval "$MAKE" --version
-eval "$CC" --version
-eval "$CXX" --version
 as --version
 ld -v
 
